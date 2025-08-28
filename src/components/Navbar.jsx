69eff0b47497946f47d999c536e9c1754299c6b8
PREME <preme@MacBookAirPREME.local>
2025-08-29 04:06:00 +0700

    Responsive Dashboard page and Main app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="bg-gradient-to-r from-blue-200 to-green-200 p-4 shadow-md rounded-b-lg">
+    <nav className="bg-blue-900 p-6 text-white">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
-          className="text-blue-800 text-3xl font-bold tracking-wide transition-transform transform hover:scale-105"
+          className="text-3xl font-bold tracking-wide transition-transform transform hover:scale-105"
         >
           Mental-Health
         </Link>
-        <div className="space-x-8">
-          {/* นำ NavLink ออกและใช้ Link โดยตรง พร้อมใส่ style */}
+
+        {/* Mobile menu */}
+        <div className="lg:hidden">
+          <button
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+            className="transition duration-300 hover:scale-120"
+          >
+            {isOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+
+        {/* Desktop menu */}
+        <div className="hidden lg:flex space-x-8">
           <Link
             to="/form"
-            className="text-blue-700 hover:text-blue-900 px-3 py-2 rounded-lg text-lg font-medium transition duration-300 ease-in-out hover:bg-white hover:shadow-md"
+            className="text-white hover:text-blue-900 px-3 py-2 rounded-lg text-lg font-medium transition duration-300 ease-in-out hover:bg-white hover:shadow-md"
           >
             แบบประเมินสุขภาพ
           </Link>
           <Link
             to="/hotline"
-            className="text-blue-700 hover:text-blue-900 px-3 py-2 rounded-lg text-lg font-medium transition duration-300 ease-in-out hover:bg-white hover:shadow-md"
+            className="text-white hover:text-blue-900 px-3 py-2 rounded-lg text-lg font-medium transition duration-300 ease-in-out hover:bg-white hover:shadow-md"
           >
             สายด่วนสุขภาพจิต
           </Link>
           <Link
             to="/ebook"
-            className="text-blue-700 hover:text-blue-900 px-3 py-2 rounded-lg text-lg font-medium transition duration-300 ease-in-out hover:bg-white hover:shadow-md"
+            className="text-white hover:text-blue-900 px-3 py-2 rounded-lg text-lg font-medium transition duration-300 ease-in-out hover:bg-white hover:shadow-md"
           >
-            ข้อมูลสุขภาพจิต
+            E-book
+          </Link>
+        </div>
+      </div>
+
+      {/* Mobile menu (Expanded) */}
+      <div className={`lg:hidden ${isOpen ? "flex" : "hidden"}`}>
+        <div className="flex flex-col mt-6 mx-auto space-y-2">
+          <Link
+            to="/form"
+            className=" hover:text-blue-900 px-30 py-2 rounded-lg text-lg font-medium transition hover:scale-105 duration-300 ease-in-out hover:bg-white hover:shadow-md text-center"
+            onClick={() => setIsOpen(false)}
+          >
+            แบบประเมินสุขภาพ
+          </Link>
+          <Link
+            to="/hotline"
+            className=" hover:text-blue-900 px-30 py-2 rounded-lg text-lg font-medium transition hover:scale-105 duration-300 ease-in-out hover:bg-white hover:shadow-md text-center"
+            onClick={() => setIsOpen(false)}
+          >
+            สายด่วนสุขภาพจิต
+          </Link>
+          <Link
+            to="/ebook"
+            className=" hover:text-blue-900 px-30 py-2 rounded-lg text-lg font-medium transition hover:scale-105 duration-300 ease-in-out hover:bg-white hover:shadow-md text-center"
+            onClick={() => setIsOpen(false)}
+          >
+            อ่าน E-book ออนไลน์
           </Link>
         </div>
       </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,141 +1,189 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
+  const [isSldebar, setIsSidebar] = useState(false);
   return (
-    <div className="flex h-screen bg-gray-100 font-sans">
+    <div className="flex flex-col md:flex-row h-screen bg-gray-100">
+      {/* Mobile Sidebar Toggle Button */}
+      {/* <div className="bg-blue-400 text-white p-4 flex justify-between items-center md:hidden">
+        <h2 className="text-xl font-bold">Dashboard</h2>
+        <button onClick={() => setIsSidebar(!isSldebar)}>
+          {isSldebar ? (
+            
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
+        </button>
+      </div> */}
+
       {/* Sidebar */}
-      <aside className="w-64 bg-gradient-to-br from-indigo-800 to-blue-900 text-white shadow-xl flex flex-col p-6 rounded-r-2xl">
-        <div className="flex items-center mb-10 mt-4">
-          <span className="text-4xl">📊</span>
-          <h2 className="text-3xl font-bold ml-3 tracking-wide">แดชบอร์ด</h2>
+      <aside className="w-full md:w-64 bg-blue-900 text-white shadow-xl flex flex-col p-4">
+        <div className="flex items-center mb-4 md:mb-10 mt-4 justify-between md:justify-start">
+          <h2 className="text-3xl font-bold md:ml-3 tracking-wide">
+            Dashboard
+          </h2>
+          <button
+            onClick={() => setIsSidebar(!isSldebar)}
+            className="md:hidden transition duration-300 hover:scale-120"
+          >
+            {isSldebar ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            )}
+          </button>
         </div>
-        <nav className="flex-1">
-          <ul className="space-y-3">
+
+        <nav
+          className={`flex-1 md:flex-col flex flex-wrap md:flex-nowrap justify-center md:justify-start ${
+            isSldebar ? "flex" : "hidden"
+          } md:flex`}
+        >
+          <ul className="space-x-2 md:space-x-0 space-y-2 md:space-y-3">
             <li>
               <Link
-                to="/"
-                className="flex items-center py-3 px-4 rounded-lg text-lg font-medium hover:bg-indigo-700 transition duration-200"
+                to="/ebook"
+                className="flex py-3 md:px-4 px-30 text-lg font-medium hover:bg-blue-950 transition duration-200 hover:scale-105"
               >
-                {/* <HomeIcon className="h-6 w-6 mr-3" /> */}
-                หน้าแรก
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/info" // ลิงก์ไปยังหน้ารายการ E-book
-                className="flex items-center py-3 px-4 rounded-lg text-lg font-medium hover:bg-indigo-700 transition duration-200"
-              >
-                {/* <BookOpenIcon className="h-6 w-6 mr-3" /> */}
                 จัดการ E-book
               </Link>
             </li>
+
             <li>
-              <button
-                onClick={() => alert('ฟังก์ชันจัดการผู้ใช้ยังไม่พร้อมใช้งาน')}
-                className="flex items-center w-full text-left py-3 px-4 rounded-lg text-lg font-medium hover:bg-indigo-700 transition duration-200"
+              <Link
+                to="/ebook"
+                className="flex py-3 md:px-4 px-30 text-lg font-medium hover:bg-blue-950 transition duration-200 hover:scale-105"
               >
-                {/* <UserGroupIcon className="h-6 w-6 mr-3" /> */}
-                จัดการผู้ใช้
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => alert('ฟังก์ชันจัดการแบบประเมินยังไม่พร้อมใช้งาน')}
-                className="flex items-center w-full text-left py-3 px-4 rounded-lg text-lg font-medium hover:bg-indigo-700 transition duration-200"
-              >
-                {/* <DocumentTextIcon className="h-6 w-6 mr-3" /> */}
                 จัดการแบบประเมิน
-              </button>
+              </Link>
             </li>
+
             <li>
-              <button
-                onClick={() => alert('ฟังก์ชันรายงานยังไม่พร้อมใช้งาน')}
-                className="flex items-center w-full text-left py-3 px-4 rounded-lg text-lg font-medium hover:bg-indigo-700 transition duration-200"
+              <Link
+                to="/ebook"
+                className="flex py-3 md:px-4 px-30 text-lg font-medium hover:bg-blue-950 transition duration-200 hover:scale-105"
               >
-                {/* <ChartBarIcon className="h-6 w-6 mr-3" /> */}
                 รายงานและ Log
-              </button>
+              </Link>
             </li>
+
+            <div className="mt-auto pt-6 border-t border-indigo-700 text-gray-300 text-sm">
+              <div className="text-center">
+                <button className="bg-red-500 px-4 py-2 rounded-sm cursor-pointer">
+                  LogOut
+                </button>
+              </div>
+            </div>
           </ul>
         </nav>
-        <div className="mt-auto pt-6 border-t border-indigo-700 text-gray-300 text-sm">
-          <p>&copy; {new Date().getFullYear()} สุขใจ. All rights reserved.</p>
-        </div>
       </aside>
 
       {/* Main Content */}
-      <main className="flex-1 p-10 overflow-auto">
-        <div className="bg-white p-10 rounded-2xl shadow-xl border border-gray-200 min-h-full">
-          <h2 className="text-5xl font-extrabold text-blue-800 mb-6 text-center">
-            ยินดีต้อนรับสู่ แดชบอร์ดผู้ดูแลระบบ
+      <main className="flex-1 p-4 lg:p-10 overflow-auto">
+        <div className="bg-white p-4 lg:p-10 rounded-2xl shadow-xl border border-gray-200 min-h-full">
+          <h2 className="text-3xl lg:text-5xl font-extrabold text-blue-800 mb-4 lg:mb-6 text-center">
+            Welcome to Dashboard
           </h2>
-          <p className="text-xl text-gray-700 mb-10 text-center leading-relaxed max-w-3xl mx-auto">
+          <p className="text-lg lg:text-xl text-gray-700 mb-6 md:mb-10 text-center leading-relaxed max-w-3xl mx-auto">
             ใช้แถบนำทางด้านซ้ายเพื่อเข้าถึงส่วนต่างๆ ของระบบจัดการเว็บไซต์
             คุณสามารถจัดการ E-book, ผู้ใช้, แบบประเมิน และดูรายงานได้ที่นี่
           </p>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-8 mt-6 md:mt-10">
             {/* การ์ดสำหรับจัดการ E-book */}
-            <div className="bg-gradient-to-br from-green-50 to-green-100 p-8 rounded-xl shadow-lg border border-green-200 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 flex flex-col justify-between">
+            <div className="bg-green-100 p-6 lg:p-8 rounded-xl shadow-lg border border-green-200 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 flex flex-col justify-between">
               <div>
-                <h3 className="text-3xl font-bold text-green-700 mb-4 flex items-center">
-                  {/* <BookOpenIcon className="h-8 w-8 mr-3" /> */}
+                <h3 className="text-2xl lg:text-3xl font-bold text-green-700 mb-2 md:mb-4 flex items-center">
                   จัดการ E-book
                 </h3>
-                <p className="text-gray-700 mb-6">
+                <p className="text-gray-700 mb-4 lg:mb-6 text-sm lg:text-base">
                   เพิ่ม, แก้ไข, ลบ E-book หรือดูรายการ E-book ทั้งหมดในระบบ
                 </p>
               </div>
-              <div className="flex space-x-4 mt-auto">
+              <div className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-4  mt-auto">
                 <Link
                   to="/info"
-                  className="bg-green-600 hover:bg-green-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+                  className="bg-green-600 hover:bg-green-700 text-white font-semibold py-3 px-4 lg:px-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 text-center"
                 >
                   ดู E-book
                 </Link>
                 <Link
                   to="/add-ebook"
-                  className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+                  className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 text-center"
                 >
                   เพิ่ม E-book
                 </Link>
               </div>
             </div>
 
-            {/* การ์ดสำหรับจัดการผู้ใช้ */}
-            <div className="bg-gradient-to-br from-purple-50 to-purple-100 p-8 rounded-xl shadow-lg border border-purple-200 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 flex flex-col justify-between">
-              <div>
-                <h3 className="text-3xl font-bold text-purple-700 mb-4 flex items-center">
-                  {/* <UserGroupIcon className="h-8 w-8 mr-3" /> */}
-                  จัดการผู้ใช้
-                </h3>
-                <p className="text-gray-700 mb-6">
-                  ดูข้อมูลผู้ใช้, จัดการสิทธิ์การเข้าถึง, หรือลบบัญชีผู้ใช้
-                </p>
-              </div>
-              <button
-                onClick={() => alert('ฟังก์ชันจัดการผู้ใช้ยังไม่พร้อมใช้งาน')}
-                className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 mt-auto"
-              >
-                จัดการผู้ใช้
-              </button>
-            </div>
-
             {/* การ์ดสำหรับจัดการแบบประเมิน */}
-            <div className="bg-gradient-to-br from-yellow-50 to-yellow-100 p-8 rounded-xl shadow-lg border border-yellow-200 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 flex flex-col justify-between">
+            <div className="bg-yellow-100 p-6 md:p-8 rounded-xl shadow-lg border border-yellow-200 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 flex flex-col justify-between">
               <div>
-                <h3 className="text-3xl font-bold text-yellow-700 mb-4 flex items-center">
-                  {/* <DocumentTextIcon className="h-8 w-8 mr-3" /> */}
+                <h3 className="text-2xl lg:text-3xl font-bold text-yellow-700 mb-2 lg:mb-4 flex items-center">
                   จัดการแบบประเมิน
                 </h3>
-                <p className="text-gray-700 mb-6">
+                <p className="text-gray-700 mb-4 lg:mb-6 text-sm lg:text-base">
                   สร้าง, แก้ไขแบบประเมินสุขภาพ หรือดูผลลัพธ์ของผู้ใช้
                 </p>
               </div>
               <button
-                onClick={() => alert('ฟังก์ชันจัดการแบบประเมินยังไม่พร้อมใช้งาน')}
+                onClick={() =>
+                  alert("ฟังก์ชันจัดการแบบประเมินยังไม่พร้อมใช้งาน")
+                }
                 className="bg-yellow-600 hover:bg-yellow-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 mt-auto"
               >
                 จัดการแบบประเมิน
@@ -143,35 +191,28 @@ const Dashboard = () => {
             </div>
 
             {/* การ์ดสำหรับดู Log/Activity */}
-            <div className="bg-gradient-to-br from-gray-50 to-gray-100 p-8 rounded-xl shadow-lg border border-gray-200 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 flex flex-col justify-between">
+            <div className="bg-gray-100 p-6 lg:p-8 rounded-xl shadow-lg border border-gray-200 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 flex flex-col justify-between">
               <div>
-                <h3 className="text-3xl font-bold text-gray-700 mb-4 flex items-center">
+                <h3 className="text-2xl lg:text-3xl font-bold text-gray-700 mb-2 lg:mb-4 flex items-center">
                   {/* <ChartBarIcon className="h-8 w-8 mr-3" /> */}
                   รายงานและ Log
                 </h3>
-                <p className="text-gray-700 mb-6">
+                <p className="text-gray-700 mb-4 lg:mb-6 text-sm lg:text-base">
                   ดูบันทึกกิจกรรม, สถิติการใช้งาน หรือรายงานต่างๆ
                 </p>
               </div>
               <button
-                onClick={() => alert('ฟังก์ชันรายงานยังไม่พร้อมใช้งาน')}
+                onClick={() => alert("ฟังก์ชันรายงานยังไม่พร้อมใช้งาน")}
                 className="bg-gray-600 hover:bg-gray-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 mt-auto"
               >
                 ดูรายงาน
               </button>
             </div>
           </div>
-
-          <div className="mt-16 p-8 bg-blue-50 rounded-xl border border-blue-200 text-blue-800 text-center shadow-inner">
-            <p className="font-semibold text-lg">
-              <span className="text-2xl">💡</span>
-              นี่คือโครงสร้างเริ่มต้นของแดชบอร์ด คุณสามารถเพิ่มฟังก์ชันการจัดการข้อมูลสำหรับแต่ละส่วนได้ในภายหลัง
-            </p>
-          </div>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
